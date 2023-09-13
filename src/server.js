71a33d914e337d945c03b5426ab0a5397388b3c6
Arnon <arnonrdp@gmail.com>
2023-09-13 09:43:05 -0300

    Updated route to `v1`

--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ const port = process.env.PORT || 3000
 
 const configRoutes = require('./routes/configRoutes')
 const poemRoutes = require('./routes/poemRoutes')
+const swaggerRoute = require('./routes/swaggerRoute')
 
-app.use('/', poemRoutes)
+app.use('/', swaggerRoute)
+app.use('/v1', poemRoutes)
 app.use('/config', configRoutes)
 
 app.listen(port, () => {
