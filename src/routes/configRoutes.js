2bc3892c3a635152f655c22aa41ade3302fbc9a3
Arnon <arnonrdp@gmail.com>
2023-09-13 18:20:32 -0300

    Simplified config routes

--- src/routes/configRoutes.js
+++ src/routes/configRoutes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const configController = require('../controllers/configController')
 
-router.post('/create-table', configController.createTable)
+router.get('/create-table', configController.createTable)
 router.get('/list-tables', configController.listTables)
+router.get('/drop-table', configController.dropTable)
 
 module.exports = router
