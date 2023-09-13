5c3222ecd31fce3e837bbfc348e8b283c2318a49
Arnon <arnonrdp@gmail.com>
2023-09-13 18:46:58 -0300

    Added CORS

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const { Pool } = require('pg')
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 
 const configRoutes = require('./routes/configRoutes')
